d90fa653d88f8093395ef7d8ba5feb87228f00ff
Cameron <chcoltrin@gmail.com>
2025-11-30 03:00:52 -0700

    Added authState for prototype authentication tracking

--- src/app.jsx
+++ src/app.jsx
@@ -5,23 +5,28 @@ import './app.css';
 import Header from './header/header.jsx';
 import Splash from './splash/splash.jsx';
 import Dashboard from './dashboard/dashboard.jsx';
+import { AuthState } from './authState.js';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-    <div className="body">
-        <Header />
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
 
-        <Routes>
-            <Route path="/" element={<Splash />}/>
-            <Route path="/dashboard" element={<Dashboard />}/>
-        </Routes>
+    return (
+        <BrowserRouter>
+            <div className="body">
+                <Header />
 
-        <footer>
-            <p>Last updated Nov 2025.</p>
-            <span><a href="https://github.com/Allergic2Bulletz/startup">Cameron Coltrin's Github</a></span>
-        </footer>
-    </div>
-    </BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Splash userName={userName} currentAuthState={currentAuthState} onAuthChange={(userName, authState) => {setAuthState(authState);setUserName(userName);}} />}/>
+                    <Route path="/dashboard" element={<Dashboard />}/>
+                </Routes>
+
+                <footer>
+                    <p>Last updated Nov 2025.</p>
+                    <span><a href="https://github.com/Allergic2Bulletz/startup">Cameron Coltrin's Github</a></span>
+                </footer>
+            </div>
+        </BrowserRouter>
 );
 }
