3c5e9ac0eed24d0bbd0f6d5f6cdfeecca327fbd0
Cameron <chcoltrin@gmail.com>
2025-12-07 06:08:58 -0700

    Rewrote authRouter to use mongodb.

---
+++ service/database.js
@@ -0,0 +1,56 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const uri = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(uri);
+const db = client.db(config.dbName);
+const userCollection = db.collection('users');
+const bookmarkCollection = db.collection('bookmarks');
+const reminderCollection = db.collection('reminders');
+const prefCollection = db.collection('preferences');
+const authCollection = db.collection('authTokens');
+
+async function testConnection() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB: ', error);
+        process.exit(1);
+    }
+}
+testConnection(email);
+
+function getUser() {
+    return userCollection.findOne({email: email});
+}
+
+function getUserByToken(token) {
+    return authCollection.findOne({token: token}, {projection: {email: 1}});
+}
+
+function addUser(email, hashedPassword) {
+    return userCollection.insertOne({email: email, password: hashedPassword});
+}
+
+function getTokens(email) {
+    return authCollection.find({email: email, expire: { $gt: Date.now() }}).toArray();
+}
+
+function getActiveSession(email, token) {
+    return authCollection.findOne({email: email, token: token, expire: { $gt: Date.now() }});
+}
+
+function addToken(email, authToken) {
+    return authCollection.insertOne({email: email, token: authToken.token, expire: authToken.expire});
+}
+
+function removeToken(token) {
+    return authCollection.deleteOne({token: token});
+}
+
+
+
+
+
+module.exports = { db, getUser, getUserByToken, addUser, addToken, getTokens, getActiveSession, removeToken };
--- service/routers/authRouter.js
+++ service/routers/authRouter.js
@@ -3,9 +3,7 @@ const { StatusCodes } = require('../utils/network.js');
 const authRouter = express.Router();
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
-
-const users = {}; // In-memory user store (should be replaced with a database in production)
-const tokens = {}; // In-memory token store
+const dbOps = require('../database.js');
 
 class authToken {
     constructor(token) {
@@ -19,11 +17,10 @@ const devName = 'dev'
 const devPassword = 'password'
 const devBcryptPassword = bcrypt.hashSync(devPassword, 10);
 users[devName] = { password: devBcryptPassword };
-// tokens[devName] = new authToken('70a92b36-8619-4e68-b41a-ab77f36290ad');
 
 
 function createAuthCookies(res, email, token) {
-    res.cookie('auth_token', token, {
+    res.cookie('authToken', token, {
         httpOnly: true,
         secure: true,
         sameSite: 'Strict'
@@ -35,50 +32,30 @@ function createAuthCookies(res, email, token) {
     });
 }
 
-function verifySession(req) {
-    const token = req.cookies.auth_token;
-    // Check all users' token arrays for a valid token
-    // for (const email in tokens) {
-    //     if (Array.isArray(tokens[email])) {
-    //         const validToken = tokens[email].find(authToken => 
-    //             authToken.token === token && authToken.expire > Date.now()
-    //         );
-    //         if (validToken) return true;
-    //     }
-    // }
-    const userTokens = tokens[req.cookies.userName];
-    if (Array.isArray(userTokens)) {
-        return userTokens.some(authToken => {
-            if (authToken.token === token && authToken.expire > Date.now()) {
-                return true;
-            }
-        });
+async function verifySession(req) {
+    if (req.cookies.authToken && req.cookies.userName) {
+        if (await dbOps.getActiveSession(req.cookies.userName, req.cookies.authToken)) {
+            return true;
+        }
     }
     return false;
 }
 
-function getEmailByToken(token) {
-    for (const email in tokens) {
-        if (Array.isArray(tokens[email])) {
-            const foundToken = tokens[email].find(authToken => authToken.token === token);
-            if (foundToken) return email;
+async function authenticate(req, res, next) {
+    if (req.cookies.authToken && req.cookies.userName) {
+        if (await dbOps.getActiveSession(req.cookies.userName, req.cookies.authToken)) {
+            return next();
         }
-    }
-    return null;
-}
-
-function authenticate(req, res, next) {
-    if (req.cookies.auth_token && req.cookies.userName && verifySession(req)) {
-        next();
+        return res.status(StatusCodes.UNAUTHORIZED).send({ msg: 'Not authenticated' });
     } else {
-        res.status(StatusCodes.UNAUTHORIZED).send({ msg: 'Not authenticated' });
+        res.status(StatusCodes.UNAUTHORIZED).send({ msg: 'Missing authToken or userName' });
     }
 };
 
-authRouter.get('/getuser', authenticate, (req, res) => {
+authRouter.get('/getuser', authenticate, async (req, res) => {
     if (!req.cookies.userName) {
-        const email = getEmailByToken(req.cookies.auth_token);
-        createAuthCookies(res, email, req.cookies.auth_token);
+        const email = await getUserByToken(req.cookies.authToken);
+        createAuthCookies(res, email, req.cookies.authToken);
         return res.status(StatusCodes.OK).send({ userName: email });
     }
     else {
@@ -88,8 +65,8 @@ authRouter.get('/getuser', authenticate, (req, res) => {
 
 authRouter.post('/register', async (req, res) => {
     const { email, password } = req.body;
-    if (req.cookies.auth_token) {
-        if (verifySession(req)) {
+    if (req.cookies.authToken) {
+        if (await verifySession(req)) {
             return res.status(StatusCodes.BAD_REQUEST).send({ error: 'Already authenticated' });
         }
     }
@@ -97,19 +74,15 @@ authRouter.post('/register', async (req, res) => {
     if (!email || !password) {
         return res.status(StatusCodes.BAD_REQUEST).send({ error: 'Email and password are required' });
     }
-    if (users[email]) {
+    if (await getUser(email)) {
         return res.status(StatusCodes.CONFLICT).send({ error: 'User already exists' });
     }
 
     const bcryptpassword = await bcrypt.hash(password, 10);
-    users[email] = { password: bcryptpassword };
+    await addUser(email, bcryptpassword);
     
-    // Initialize token array for new user or add to existing array
-    if (!tokens[email]) {
-        tokens[email] = [];
-    }
     const newToken = new authToken(crypto.randomUUID());
-    tokens[email].push(newToken);
+    await dbOps.addToken(email, newToken);
     
     createAuthCookies(res, email, newToken.token);
     return res.status(StatusCodes.CREATED).send({ msg: 'User registered successfully' });
@@ -117,8 +90,8 @@ authRouter.post('/register', async (req, res) => {
 
 authRouter.post('/login', async (req, res) => {
     const { email, password } = req.body;
-    if (req.cookies.auth_token) {
-        if (verifySession(req)) {
+    if (req.cookies.authToken) {
+        if (await verifySession(req)) {
             return res.status(StatusCodes.BAD_REQUEST).send({ error: 'Already authenticated' });
         }
     }
@@ -126,60 +99,35 @@ authRouter.post('/login', async (req, res) => {
     if (!email || !password) {
         return res.status(StatusCodes.BAD_REQUEST).send({ error: 'Email and password are required' });
     }
-    const user = users[email];
+
+    const user = await dbOps.getUser(email);
     if (!user || !(await bcrypt.compare(password, user.password))) {
         return res.status(StatusCodes.UNAUTHORIZED).send({ error: 'Invalid email or password' });
     }
 
-    // Initialize token array for user if it doesn't exist
-    if (!tokens[email]) {
-        tokens[email] = [];
-    }
     
-    // Add new session token to user's token array
+    // Add new session token
     const newToken = new authToken(crypto.randomUUID());
-    tokens[email].push(newToken);
+    await dbOps.addToken(email, newToken);
     
     createAuthCookies(res, email, newToken.token);
     return res.status(StatusCodes.OK).send({ msg: 'User logged in successfully' });
 });
 
-authRouter.delete('/logout', authenticate, (req, res) => {
-    const token = req.cookies.auth_token;
-    if (token) {
-        // Find and remove the specific token from the user's token array
-        // for (const email in tokens) {
-        //     if (Array.isArray(tokens[email])) {
-        //         const tokenIndex = tokens[email].findIndex(authToken => authToken.token === token);
-        //         if (tokenIndex !== -1) {
-        //             tokens[email].splice(tokenIndex, 1);
-        //             // Clean up empty arrays
-        //             if (tokens[email].length === 0) {
-        //                 delete tokens[email];
-        //             }
-        //             break;
-        //         }
-        //     }
-        // }
-        const userTokens = tokens[req.cookies.userName];
-        if (Array.isArray(userTokens)) {
-            tokens[req.cookies.userName] = userTokens.filter(authToken => authToken.token !== token);
-        }
-        // Clean up empty arrays
-        if (tokens[req.cookies.userName].length === 0) {
-            delete tokens[req.cookies.userName];
-        }
-    } else {
-        return res.status(StatusCodes.BAD_REQUEST).send({ error: 'No active session' });
+authRouter.delete('/logout', authenticate, async (req, res) => {
+    const token = req.cookies.authToken;
+
+    const delResult = await dbOps.removeToken(token);
+    if (delResult.deletedCount === 0) {
+        return res.status(StatusCodes.BAD_REQUEST).send({ error: 'Token not found' });
     }
     
-    res.clearCookie('auth_token');
+    res.clearCookie('authToken');
     res.clearCookie('userName');
     return res.status(StatusCodes.OK).send({ msg: 'User logged out successfully' });
 });
 
 module.exports = {
     authRouter,
-    users,
     authenticate,
 };
