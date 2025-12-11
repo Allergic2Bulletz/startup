b8020577bff3d4f4c27f223d0e61c5775ce57e74
Cameron <chcoltrin@gmail.com>
2025-12-10 17:39:28 -0700

    Added Sync commands to prefRouter

--- service/routers/prefRouter.js
+++ service/routers/prefRouter.js
@@ -3,13 +3,19 @@ const { StatusCodes } = require('../utils/network.js');
 const { authenticate } = require('./authRouter.js')
 const crypto = require('crypto');
 const dbOps = require('../database.js');
+const { Command } = require('../wsServer.js');
 
 const prefRouter = express.Router();
+// Use a getter function to access wsServer after it's initialized
+const getWsServer = () => {
+    const { wsServer } = require('../index.js');
+    return wsServer;
+};
 
 // Get preferences
 prefRouter.get('/', authenticate, async (req, res) => {
     const prefs = await dbOps.getPreferences(req.cookies.userName);
-    res.status(StatusCodes.OK).send(prefs || {});
+    res.status(StatusCodes.OK).send({notifications: prefs.notifications, reminderSound: prefs.reminderSound, theme: prefs.theme, modifiedAt: prefs.modifiedAt} || {});
 });
 
 // Update preferences
@@ -18,10 +24,12 @@ prefRouter.put('/', authenticate, async (req, res) => {
     
     const result = await dbOps.updatePreferences(req.cookies.userName, updatedPrefs);
     
-    if (result.modifiedCount !== 1) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ msg: 'Failed to update preferences' });
+    if (result.modifiedCount == 1 || result.upsertedCount == 1) {
+        res.status(StatusCodes.OK).send(updatedPrefs);
+        getWsServer().sendToUserClients(req.cookies.userName, new Command('sync', 'preferences', {}));
+        return;
     }
-    res.status(StatusCodes.OK).send(updatedPrefs);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ msg: 'Failed to update preferences' });
 });
 
 // Delete preferences
@@ -31,6 +39,7 @@ prefRouter.delete('/', authenticate, async (req, res) => {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ msg: 'Failed to delete preferences' });
     }
     res.status(StatusCodes.OK).send({ msg: 'Preferences deleted' });
+    getWsServer().sendToUserClients(req.cookies.userName, new Command('sync', 'preferences', {}));
 });
 
 
